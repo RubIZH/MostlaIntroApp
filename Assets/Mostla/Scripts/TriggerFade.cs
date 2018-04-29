555d61d0e315815652a18f48cdcff2a35efafe00
Ruben Hidalgo <get1595@gmail.com>
2018-04-29 18:45:04 -0500

    Reticle was Fixed

---
+++ Assets/Mostla/Scripts/TriggerFade.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerFade : MonoBehaviour {
+
+
+	public CanvasGroup uiElement;
+
+	private float fadingVelocity = 0.01f;
+	private bool isFadingIn = false;
+	private bool isFadingOut = false;
+
+
+
+
+	void Update(){
+
+		if (isFadingIn) {
+			uiElement.alpha += fadingVelocity;
+			if (uiElement.alpha >= 1) {
+				isFadingIn = false;
+			}
+
+		}
+
+		if (isFadingOut) {
+			uiElement.alpha -= fadingVelocity;
+			if (uiElement.alpha <= 0)
+				isFadingOut = false;
+		}
+
+
+
+	}
+
+
+	public void TriggerFadeIn(){
+		isFadingIn = true;
+	}
+
+	public void TriggerFadeOut(){
+		isFadingOut = true;
+	}
+
+
+
+
+
+}
